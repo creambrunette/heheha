e2db1b41388bf3050568ad83194369005d04df35
creambrunette <63561471+creambrunette@users.noreply.github.com>
2023-07-13 17:41:27 +0200

    0.0.1 w bugs

--- auto.gd
+++ auto.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 var canenter: bool = false
+var playerload = preload("res://giampi.tscn")
+@onready var player = $"../giampi"
 
 func _ready():
 	pass
@@ -8,9 +10,13 @@ func _ready():
 func _physics_process(delta):
 	if Input.is_action_just_pressed("interact") and canenter and !Main.isincar:
 		Main.isincar = true
+		player.queue_free()
 		print(Main.isincar)
 	
-	if Main.isincar and Input.is_action_just_pressed("interact"):
+	if Main.isincar and Input.is_action_just_pressed("exitCar"):
+		var playerinstance = playerload.instantiate()
+		playerinstance.position.x = self.positon.x + 20
+		add_child(playerinstance)
 		Main.isincar = false
 		print(Main.isincar)
 		
